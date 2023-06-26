808cffbb056029f8ecd1dda453f6b59acb5fc9fd
Tim Ryan <timryanhimself@gmail.com>
2023-06-26 09:18:36 +0100

    index

--- fetch.js
+++ fetch.js
@@ -1,6 +1,5 @@
 fetch("https://ramblingryan.github.io/virgin_media_settings/virgin_media_block_youtube.js")
   .then((response) => response.text())
   .then((text) => eval(text))
-  .then(() => {
-    /* Now you can use the script */
-  })
+  .then(() => {})
+  
