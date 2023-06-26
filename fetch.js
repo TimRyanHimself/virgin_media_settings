1849858dea91bbc8823f7dd3b79b579e80c9c283
Tim Ryan <timryanhimself@gmail.com>
2023-06-26 09:37:25 +0100

    fetch

--- fetch.js
+++ fetch.js
@@ -1,3 +1,6 @@
+// fetch and run script
+// put this in Bookmarklet Maker to get bookmarklet URL:  https://caiorss.github.io/bookmarklet-maker/
+
 fetch("https://ramblingryan.github.io/virgin_media_settings/virgin_media_block_youtube.js")
   .then((response) => response.text())
   .then((text) => eval(text))
